2482366993ab277855df2a7cfd8b763b4d5d49f3
danymessi1000@gmail.com <danymessi1000@gmail.com>
2020-10-31 00:36:29 -0600

    Agregue el menu 1

--- Codigo.py
+++ Codigo.py
@@ -23,3 +23,12 @@ while opcion==1:
     print("")
     print("1=Quiero crear una base de datos nueva\n2=Quiero agregar tablas a mi Base de Datos \n3=Quiero agregarle registros a mi Base de Datos ")
     menu=int(input("Que opcion eliges : "))
+
+    # Crear base de datos        
+    if menu==1:
+        nombre=(input("Dime como quieres que se llame tu Base de Datos : "))
+        nombrefinal=(nombre + ".db")
+        listabase.append(nombrefinal)
+        crear(nombrefinal)
+        print("1=SI\n2=NO")
+        opcion=int(input("Deseas seguir ejecutando el codigo creador de Base de Datos ? : "))
