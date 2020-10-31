ba90a74d397061a0800c40112ef835d2c6d23d51
danymessi1000@gmail.com <danymessi1000@gmail.com>
2020-10-31 00:33:29 -0600

    Importe mis funciones al codigo principal

--- Codigo.py
+++ Codigo.py
@@ -2,3 +2,8 @@ import sys
 import sqlite3
 from sqlite3 import Error
 from plyer import notification
+
+from funciones import tipos
+from funciones import insertar
+from funciones import crear
+from funciones import agregar
